1daf9f00f6aaaedc445612e6458ca1dac1633828
vivek-docs <vivek.author@gmail.com>
2026-02-17 10:18:04 +0000

    Deploying to gh-pages from @ vivek-docs/vivek-docs.github.io@e843969b6ef003d16da671b8aa884688cde22b9f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,8 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
+  },{id: "nav-travelogue",
+          title: "travelogue",
           description: "",
           section: "Navigation",
           handler: () => {
